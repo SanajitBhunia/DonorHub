26fd433d644797cf499fbca5d3ac0d5b931d0ce7
SanajitBhunia <sanajitbhunia13@gmail.com>
2024-05-14 09:06:33 +0530

    save data function

--- app/src/main/java/com/example/donorhub/ui/auth/Donor_receiver_activity.kt
+++ app/src/main/java/com/example/donorhub/ui/auth/Donor_receiver_activity.kt
@@ -7,28 +7,50 @@ import com.example.donorhub.databinding.ActivityDonorReceiverBinding
 import com.example.donorhub.ui.MainActivity
 import com.example.donorhub.ui.MainActivity2
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class Donor_receiver_activity : AppCompatActivity() {
     private lateinit var binding: ActivityDonorReceiverBinding
     private lateinit var auth:FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityDonorReceiverBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
         auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+
+
 
 
         binding.btndonate.setOnClickListener {
-
-                    startActivity(Intent(this, RegstionActivity::class.java))
-
+            checkUserData()
 
         }
         binding.btnrequest.setOnClickListener {
             startActivity(Intent(this,MainActivity2::class.java))
         }
 
+    }
+
+    private fun checkUserData() {
+        val currentUser = auth.currentUser
+        currentUser?.let { user ->
+            firestore.collection("Donor").document(user.uid).get()
+                .addOnSuccessListener { documentSnapshot ->
+                    if (documentSnapshot.exists()) {
+                        // User data exists, proceed to another activity
+                        startActivity(Intent(this, MainActivity::class.java))
+                    } else {
+                        // User data doesn't exist, navigate to registration activity
+                        startActivity(Intent(this, RegstionActivity::class.java))
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    // Handle any errors
+                }
+        }
     }
 }
